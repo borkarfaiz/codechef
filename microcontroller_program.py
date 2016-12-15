e817e80d120b110b1d83979f61409c41e5199069
borkarfaiz <borkarfaiz@gmail.com>
2016-12-15 15:36:05 +0530

    using input in one line

--- microcontroller_program.py
+++ microcontroller_program.py
@@ -1,19 +1,14 @@
 from fractions import gcd
 lcm = lambda a, b: (a*b)//gcd(a,b)
 
-
+min = 10**5
 t = int(input())
 
 while t:
 	min = 10**5
 	no = int(input())
-	n = []	
-	"""
 	n = input().split(' ')
 	n = list(map(int, n))
-	"""
-	for i in range(no):
-		n.append(int(input()))
 	for i in range(len(n)):
 		for j in range(i+1, len(n)):
 			if(n[j]>=min):

7dae8af801b1fd1eafd798bdf16c292d2ffa050b
borkarfaiz <borkarfaiz@gmail.com>
2016-12-21 15:35:12 +0530

    my best solution for chefstud

--- chefstud.py
+++ chefstud.py
@@ -1,19 +1,9 @@
 t = int(input())
 while(t):
 	s = input()
-	ls = []
 	c = 0
-	for i in s:
-		if i == '*':
-			ls.append(i)
-		elif i == '>':
-			ls.append('<')
-		else:
-			ls.append('>')
-	for i in range(0, len(ls)-1):
-		if ls[i] == '*':
-			continue
-		elif ls[i] == '>' and ls[i+1] == '<':
-			c = c+1
+	for i in range(len(s)-1):
+		if s[i] == '<' and s[i+1] == '>':
+			c = c + 1		
 	t=t-1
 	print(c)
